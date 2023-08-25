3626b4a3b22bffc0b61f54325f1e9077b75feef7
juanvicente5 <143141771+juanvicente5@users.noreply.github.com>
2023-08-24 22:07:40 -0300

    Create apps.py

---
+++ aplicacion/apps.py
@@ -0,0 +1,6 @@
+de  Django . importación de aplicaciones  AppConfig 
+
+
+clase  AplicacionConfig ( AppConfig ):
+    default_auto_field  =  'django.db.models.BigAutoField'
+    nombre  =  'aplicación'
